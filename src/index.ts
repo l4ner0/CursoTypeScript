48ba016d11d7059140241d8ee5c67135fc6e98a8
Diego Urbina <diego.urbina@acceso.com.pe>
2022-07-10 16:18:35 -0500

    02. Tipos de variables

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-const userName = 'Dominicode';
+// Basic types
+let myTypeString: string = 'Hello World!';
+let myTypeNumber: number = 33;
+let myTypeBoolean: boolean = true;
+
+// Array
+let arrayNumber: number[] = [1, 2, 3];
+let arrayNumber2: Array<number> = [1, 2, 3];
+
+let arrayString: String[] = ['1', '2'];
+let arrayString2: Array<String> = ['Uno', 'Dos'];
+
+let arrayAny: any[] = ['hola', true, 3];
+let arrayAny2: Array<any> = [2, 'hola', false];
+
+// Tuple
+let tuplaPlayers: [string, number, boolean] = ['Doncic', 5, true];
+
+// Tuple Array
+let players: [number, string][];
+
+players = [
+    [1, 'Lebron'],
+    [2, 'Durand'],
+    [3, 'Carry']
+];
