d26a474722e06e15b2a8fe3c2aec64105be37c52
Diego Urbina <diego.urbina@acceso.com.pe>
2022-07-29 21:55:57 -0500

    name spaces

--- dist/typeAssertion.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// Type assertion
-let channel = 'Dominicode';
-/* let channelStr = <String>channel; */
-let channelStr = channel;
-const myCanvas = document.getElementById('main');
-const myCanvas1 = document.getElementById('main');
-const myCanvas2 = document.getElementById('main');
---
+++ src/utility.ts
@@ -0,0 +1,11 @@
+namespace Utility {
+    export namespace Taxes {
+        export function calculateIva(price: number): number {
+            return (price * 0.21) + price;
+        }
+
+        export function calculatePenaltyIva (price: number): number {
+            return (price * 0.3) + price;
+        }
+    }
+}
